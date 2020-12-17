d9ce60d35fec0c7db915060462736a080523e393
Mike Grammes <mike.grammes@gmail.com>
2020-12-17 15:07:22 -0500

    changed projectile folder

--- init.el
+++ init.el
@@ -113,8 +113,8 @@
   :bind-keymap
   ("C-c p" . projectile-command-map)
   :init
-  (when (file-directory-p "~/Projects/Code")
-    (setq projectile-project-search-path '("~/Projects/Code")))
+  (when (file-directory-p "~/github")
+    (setq projectile-project-search-path '("~/github")))
   (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package counsel-projectile
@@ -123,13 +123,14 @@
 ;; set up magit for git integration
 (use-package magit)
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit counsel-projectile all-the-icons general doom-themes helpful ivy-rich counsel ivy command-log-mode commang-log-mode use-package)))
+   '(forge magit counsel-projectile all-the-icons general doom-themes helpful ivy-rich counsel ivy command-log-mode commang-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- transient/history.el
+++ transient/history.el
@@ -1,5 +1,7 @@
 ((magit-branch nil)
  (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
  (magit-dispatch nil)
  (magit-log
   ("-n256" "--graph" "--decorate"))
