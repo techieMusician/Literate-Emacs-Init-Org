3c6ba86a82e10b45817f7e91d92da70d4de688c6
Mike Grammes <mike.grammes@gmail.com>
2021-01-03 17:02:14 -0500

    Added emacsroam support and org changes commit sooner

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
 (scroll-bar-mode -1)         ; Disable visable Scrollbar
 (tool-bar-mode -1)           ; Disable toolbar
 (tooltip-mode -1)            ; Disable tooltips
-(set-fringe-mode 10)         ; Give space around emacs
+;(set-fringe-mode 10)         ; Give space around emacs
 (menu-bar-mode -1)           ; Get rid of menubar
 
 ;; Set a pretty theme
@@ -30,6 +30,16 @@
                 shell-command-with-editor-mode-hook
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode -1))))
+
+;;;; Font Configuration
+(defvar runemacs/default-font-size 180)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height runemacs/default-font-size)
+
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 260)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 295 :weight 'regular)
 
 ;;; Manage packages with use-package
 
@@ -125,14 +135,63 @@
 ;; look into force for github integration
 
 ;;;; ORGMODE SETTINGS
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
 ;;; Setu orgmode
 (use-package org
+  :hook (org-mode . efs/org-mode-setup)
   :config
-  (setq org-ellipsis " ^"))
+  (setq org-ellipsis " ▾")
+					; (setq org-ellipsis " ^")
+  (efs/org-font-setup))
+
+;;;; Setup org-roam
+(use-package org-roam
+  :config
+  (setq org-roam-directory "~/org")
+  (add-hook 'after-init-hook 'org-roam-mode))
 
 (use-package org-bullets
   :after org
   :hook (org-mode . org-bullets-mode))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+	visual-fill-column-center-text t)
+  (visual-fill-column-mode t))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;DON'T CHANGE THIS
@@ -143,7 +202,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-bullets which-key use-package rainbow-delimiters ivy-rich helpful general forge doom-themes counsel-projectile command-log-mode all-the-icons)))
+   '(unicode-fonts org-roam visual-fill-column org-bullets which-key use-package rainbow-delimiters ivy-rich helpful general forge doom-themes counsel-projectile command-log-mode all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
