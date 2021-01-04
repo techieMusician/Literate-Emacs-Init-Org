0819bf592c68401e0d5f03deb2f0ebea06c39b95
Mike Grammes <mike.grammes@gmail.com>
2021-01-04 15:55:35 -0500

    Actually got todo keywords working.

--- init.el
+++ init.el
@@ -193,7 +193,8 @@
   (require 'org-habit)
   (add-to-list 'org-modules 'org-habit)
   (setq org-habit-graph-column 60)
-  
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")))
   (require 'org-protocol)
   (efs/org-font-setup))
 
