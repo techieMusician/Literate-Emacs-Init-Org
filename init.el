22049a86201ee58c19f5daf3706b2ba8606d90f4
Mike Grammes <mike.grammes@gmail.com>
2021-01-04 17:39:23 -0500

    added time logging to all state changes (for now)

--- init.el
+++ init.el
@@ -194,7 +194,7 @@
   (add-to-list 'org-modules 'org-habit)
   (setq org-habit-graph-column 60)
   (setq org-todo-keywords
-	'((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d@/!)")))
+	'((sequence "TODO(t!)" "NEXT(n!)" "|" "DONE(d@/!)")))
   (setq org-refile-targets
 	'(("archives.org" :maxlevel . 1)
 	  ("tasks.org" :maxlevel . 1)
