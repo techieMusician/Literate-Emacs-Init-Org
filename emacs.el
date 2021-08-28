3c496145a742dd8a27684d3cf44dc539a8c43ddf
Mike Grammes <mike.grammes@gmail.com>
2021-08-28 01:02:27 -0400

    Remove Custom settings from emacs.org
    These settings are auto saving in init.el so I don't need them in
    emacs.org because there should only be one. May need to look into this
    more later

--- emacs.el
+++ emacs.el
@@ -248,17 +248,3 @@
 (add-hook 'org-mode-hook
           (lambda () (add-hook 'after-save-hook #'org-babel-tangle
                           :append :local)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(zetteldeft deft doom-modeline org-roam-protocol unicode-fonts org-roam visual-fill-column org-bullets which-key use-package rainbow-delimiters ivy-rich helpful general forge doom-themes counsel-projectile command-log-mode all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
